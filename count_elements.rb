7055ee87ba76f7fb35fd87098524fa722cd3e260
Matthew Bartl <mattbartl@gmail.com>
2019-04-10 17:46:20 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,8 +3,11 @@ require 'pry'
 def count_elements(array)
   new_hash = {}
   array.map do |item| 
-    new_hash.has_key?(item) {new_hash[item] += 1}
-    !new_hash.has_key?(item) {new_hash[item] = 1}
+    if new_hash.has_key?(item) 
+      new_hash[item] += 1 
+    else
+      new_hash[item] = 1
+    end
   end
   new_hash
 end
