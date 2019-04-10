cdd40abac5cec7dd034571025205558e78a4bf63
Matthew Bartl <mattbartl@gmail.com>
2019-04-10 17:20:32 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,10 +1,10 @@
 def count_elements(array)
   new_hash = {}
-  array.each do |array|
-    if new_hash.has_key?(array) 
-      new_hash[array] += 1 
+  array.each do |array_i|
+    if new_hash.has_key?(array_i) 
+      new_hash[array_i] += 1 
     else
-      new_hash[array] = 1
+      new_hash[array_i] = 1
     end
   end
 end
