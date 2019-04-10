8645bc17a2f04209c5eeb1544d0a76cb56102a37
Matthew Bartl <mattbartl@gmail.com>
2019-04-10 17:28:28 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,9 +3,9 @@ def count_elements(array)
   i = 0
   while i < array.length do
     if new_hash.has_key?(array[i])
-      new_hash[array[i]] += 1
+      new_hash["#{array[i]}"] += 1
     else
-      new_hash.merge{array[i]: 1}
+      new_hash.merge{"#{array[i]}": 1}
     end
     i += 1
   end
