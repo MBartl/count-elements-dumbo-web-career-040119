cdd78c33a27478410887ae256db37acaafbf97b6
Matthew Bartl <mattbartl@gmail.com>
2019-04-10 17:35:56 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -4,7 +4,11 @@ def count_elements(array)
   new_hash = {}
   i = 0
   while i < array.length do
-    new_hash[array[i]] = 1
+    if !new_hash.has_key(array[i])
+      new_hash[array[i]] = 1
+    else
+      new_hash[array[i]] += 1
+    end
     i += 1
   end
   binding.pry
